05c864dabbb642f58529a93102d697a3f84344cd
Daniel Arcos <91172879+Daniel-Arcos@users.noreply.github.com>
2024-05-08 00:47:25 -0600

    Diseño de UI para Cambiar contraseña

--- app/src/main/java/com/sdi/hostedin/feature/user/ProfileActivity.java
+++ app/src/main/java/com/sdi/hostedin/feature/user/ProfileActivity.java
@@ -4,24 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.sdi.hostedin.R;
 import com.sdi.hostedin.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ActivityProfileBinding binding;
+    private BottomSheetBehavior bottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        RelativeLayout bottomSheet = findViewById(R.id.standard_bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        bottomSheetBehavior.setDraggable(false);
+        binding.changePasswordBtn.setOnClickListener(v -> bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED));
+        binding.cancelChangePasswordBtn.setOnClickListener(v -> bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN));
         binding.myAccountBtn.setOnClickListener( v -> openEditProfileActivity());
         binding.deleteAccountBtn.setOnClickListener( v -> openDeleteAccountActivity());
     }
-
     private void openEditProfileActivity() {
         Intent intent = new Intent(this, EditProfileActivity.class);
         startActivity(intent);
