80a09bb4146ff0ef3992319e7aa12377f0073d55
Eduardo García Díaz <eduardo.gd17@gmail.com>
2024-05-23 22:11:08 -0600

    validation on bookings dates

--- app/src/main/java/com/sdi/hostedin/feature/guest/bookings/accommodationbooking/AccommodationBookingViewModel.java
+++ app/src/main/java/com/sdi/hostedin/feature/guest/bookings/accommodationbooking/AccommodationBookingViewModel.java
@@ -7,19 +7,23 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import com.sdi.hostedin.data.callbacks.BookingCallback;
+import com.sdi.hostedin.data.callbacks.BookingsCallback;
 import com.sdi.hostedin.data.datasource.local.DataStoreAccess;
 import com.sdi.hostedin.data.model.Booking;
 import com.sdi.hostedin.domain.CreateAccommodationUseCase;
 import com.sdi.hostedin.domain.CreateBookingUseCase;
+import com.sdi.hostedin.domain.GetBookingsOfAccommodationUseCase;
 import com.sdi.hostedin.ui.RequestStatus;
 import com.sdi.hostedin.ui.RequestStatusValues;
 import com.sdi.hostedin.utils.DateFormatterUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.TimeZone;
 
 public class AccommodationBookingViewModel extends AndroidViewModel {
+    public static final String SUCCESS_BOOKING_MESSAGE  = "booking created";
     private MutableLiveData<RequestStatus> requestStatusMutableLiveData = new MutableLiveData<>();
     private MutableLiveData<Booking> bookingMutableLiveData = new MutableLiveData<>();
     private MutableLiveData<Integer> realGuestsNumber = new MutableLiveData<>();
@@ -59,6 +63,11 @@ public class AccommodationBookingViewModel extends AndroidViewModel {
 
     public MutableLiveData<Double> getTotalCost() {
         return totalCost;
+    }
+    private MutableLiveData<List<Booking>> bookingsList = new MutableLiveData<>();
+
+    public MutableLiveData<List<Booking>> getBookingsList() {
+        return bookingsList;
     }
 
     public void incrementRealGuestsNumber(int guestsLimit) {
@@ -111,7 +120,7 @@ public class AccommodationBookingViewModel extends AndroidViewModel {
             @Override
             public void onSuccess(Booking booking, String token) {
                 bookingMutableLiveData.setValue(booking);
-                requestStatusMutableLiveData.setValue(new RequestStatus(RequestStatusValues.DONE, "booking created"));
+                requestStatusMutableLiveData.setValue(new RequestStatus(RequestStatusValues.DONE, SUCCESS_BOOKING_MESSAGE));
             }
 
             @Override
@@ -119,6 +128,24 @@ public class AccommodationBookingViewModel extends AndroidViewModel {
                 requestStatusMutableLiveData.setValue(new RequestStatus(RequestStatusValues.ERROR, errorMessage));
             }
         });
+    }
 
+    public void getAccommodationBookings(String accommodationId) {
+        GetBookingsOfAccommodationUseCase getBookingsOfAccommodation = new GetBookingsOfAccommodationUseCase();
+
+        requestStatusMutableLiveData.setValue(new RequestStatus(RequestStatusValues.LOADING, ""));
+        String token = DataStoreAccess.accessToken(getApplication());
+        getBookingsOfAccommodation.getBookingsOfSpecificAccommodation(accommodationId, token, new BookingsCallback() {
+            @Override
+            public void onSuccess(List<Booking> bookingList, String message) {
+                requestStatusMutableLiveData.setValue(new RequestStatus(RequestStatusValues.DONE, message));
+                bookingsList.setValue(bookingList);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                requestStatusMutableLiveData.setValue(new RequestStatus(RequestStatusValues.ERROR, errorMessage));
+            }
+        });
     }
 }
